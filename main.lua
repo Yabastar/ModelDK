b87a8cba43f1d2043281b21ae4960121a3524a9b
Yabastar <138197982+Yabastar@users.noreply.github.com>
2024-02-02 19:24:55 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local Pine3D = require("/Pine3D")
+local PrimeUI = require("/PrimeUI")
 
 local ThreeDFrame = Pine3D.newFrame()
 ThreeDFrame:setCamera(-1, 0, -1, 0, 45, -45)
@@ -15,21 +16,29 @@ for x = 1, size do
   end
 end
 
-while true do
-  -- render the scene
-  ThreeDFrame:drawObjects(objects)
-  ThreeDFrame:drawBuffer()
-
-  -- wait for user input
-  local event, key, x, y = os.pullEvent()
-
-  -- check if the event was a click
-  if event == "mouse_click" then
-    -- pass the rendered objects with the coordinates of the clicked pixel
-    local objectIndex, polyIndex = ThreeDFrame:getObjectIndexTrace(objects, x, y)
-    if objectIndex then -- check if an object was clicked
-	  -- remove the object from the list of rendered objects
-      table.remove(objects, objectIndex)
-    end
-  end
+local function main()
+	while true do
+	  -- render the scene
+	  ThreeDFrame:drawObjects(objects)
+	  ThreeDFrame:drawBuffer()
+	
+	  -- wait for user input
+	  local event, key, x, y = os.pullEvent()
+		
+	  -- check if the event was a click
+	  if event == "mouse_click" then
+	    -- pass the rendered objects with the coordinates of the clicked pixel
+	    local objectIndex, polyIndex = ThreeDFrame:getObjectIndexTrace(objects, x, y)
+	    if objectIndex then -- check if an object was clicked
+		clicked = [objectIndex,polyIndex]
+	    end
+	  end
+	end
 end
+
+local function ui()
+	PrimeUI.clear()
+	PrimeUI.borderBox(term.current(), 4, 6, 40, 10)
+end
+
+parallel.waitForAny(main,ui)
